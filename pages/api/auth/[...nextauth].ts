8960908ea6a2aadc69e002a7e0306504c69f05aa
Your Name <roberttoribio04@gmail.com>
2025-06-06 21:55:22 -0700

    adds prisma schemas and first migration to neon

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,12 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import {PrismaAdapter} from "@auth/prisma-adapter";
+import {PrismaClient} from "@prisma/client";
+import {AuthOptions} from "next-auth";
 
-export const authOptions = {
+const prisma = new PrismaClient();
+export const authOptions:AuthOptions = {
+  adapter:PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
@@ -9,6 +14,10 @@ export const authOptions = {
     }),
   ],
   // add other NextAuth options here
+  session:{
+    strategy:"database",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
 };
 
 export default NextAuth(authOptions);

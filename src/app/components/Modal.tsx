22ec1411703326e7357140008791d554e3de4426
Your Name <roberttoribio04@gmail.com>
2025-06-11 16:33:45 -0700

    fixes modal by centering image and makes exit button white

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -15,13 +15,13 @@ const Modal: React.FC<ModalProps> = ({isOpen, onClose, children}) =>{
     if(!isOpen) return null;
     return(
         <div className="fixed inset-0 z-50 flex items-center 
-        justify-cneter bg-black bg-opacity-50">
+        justify-center bg-black bg-opacity-50">
 
         <button 
         onClick={onClose}
-        className='absolute top-4 right-4 text-gray-600
+        className='absolute top-5 right-7 text-white
         hover:text-gray-900
-        text-2xl'
+        text-4xl'
         aria-label="Close modal">
             <FaTimes></FaTimes>
         </button>
